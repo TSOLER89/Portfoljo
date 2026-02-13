5df69f14f818bf82a977997ab611b75c88a01d30
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-02-13 09:48:28 +0100

    Add home.js to implement the home page rendering and floating logos

---
+++ pages/home.js
@@ -0,0 +1,18 @@
+import { createFloatingLogos } from "../ui/floatingLogos.js";
+
+export function renderHome() {
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+        <section class="hero">
+            <div class="home-content">
+                <img src="Assets/Photos/CvBild.jpg" class="profile-image">
+                <h2>Hej! Välkommen till min portfolio!</h2>
+                <p>Här kan du läsa mer om mig, bläddra igenom mina projekt och kontakta mig.</p>
+            </div>
+        </section>
+        <div class="floating-logos" id="floatingLogos"></div>
+    `;
+
+    createFloatingLogos();
+}
